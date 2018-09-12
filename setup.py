c431ac55c8ab5fc0885b132cd2190a0780795b09
Adam Savitzky <adam.savitzky@gmail.com>
2018-09-12 14:51:50 -0700

    Bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name="flipper-client",
-    version="0.2.3",
+    version="0.2.4",
     packages=find_packages(),
     license="MIT",
     long_description=open("README.md").read(),
