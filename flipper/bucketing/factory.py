502fdda317d565d867ab737b083fc891b9884e75
Adam Savitzky <adam.savitzky@gmail.com>
2018-09-12 09:47:01 -0700

    Ignore type errors for python 3.5.2 compatibility

--- flipper/bucketing/factory.py
+++ flipper/bucketing/factory.py
@@ -1,13 +1,9 @@
-from typing import Any, Dict, Type, Union
+from typing import Any, Dict
 
 from .base import AbstractBucketer
 from .consistent_hash_percentage_bucketer import ConsistentHashPercentageBucketer
 from .noop_bucketer import NoOpBucketer
 from .percentage_bucketer import PercentageBucketer
-
-BucketerTypes = Union[
-    Type[ConsistentHashPercentageBucketer], Type[NoOpBucketer], Type[PercentageBucketer]
-]
 
 
 class BucketerFactory:
@@ -15,7 +11,7 @@ class BucketerFactory:
         ConsistentHashPercentageBucketer.get_type(): ConsistentHashPercentageBucketer,
         NoOpBucketer.get_type(): NoOpBucketer,
         PercentageBucketer.get_type(): PercentageBucketer,
-    }  # type: Dict[str, BucketerTypes]
+    }
 
     class InvalidBucketerTypeError(Exception):
         pass
@@ -23,7 +19,7 @@ class BucketerFactory:
     @classmethod
     def create(cls, fields: Dict[str, Any]) -> AbstractBucketer:
         try:
-            return cls.BUCKETER_MAP[fields["type"]].from_dict(fields)
+            return cls.BUCKETER_MAP[fields["type"]].from_dict(fields)  # type: ignore
         except KeyError:
             raise cls.InvalidBucketerTypeError(
                 "Bucketer type not supported: %s" % fields["type"]
--- flipper/bucketing/percentage/factory.py
+++ flipper/bucketing/percentage/factory.py
@@ -1,17 +1,15 @@
-from typing import Any, Dict, Type, Union
+from typing import Any, Dict
 
 from .base import AbstractPercentage
 from .linear_ramp_percentage import LinearRampPercentage
 from .percentage import Percentage
-
-PercentageTypes = Union[Type[LinearRampPercentage], Type[Percentage]]
 
 
 class PercentageFactory:
     PERCENTAGE_MAP = {
         LinearRampPercentage.get_type(): LinearRampPercentage,
         Percentage.get_type(): Percentage,
-    }  # type: Dict[str, PercentageTypes]
+    }
 
     class InvalidPercentageTypeError(Exception):
         pass
@@ -19,7 +17,7 @@ class PercentageFactory:
     @classmethod
     def create(cls, fields: Dict[str, Any]) -> AbstractPercentage:
         try:
-            return cls.PERCENTAGE_MAP[fields["type"]].from_dict(fields)
+            return cls.PERCENTAGE_MAP[fields["type"]].from_dict(fields)  # type: ignore
         except KeyError:
             raise cls.InvalidPercentageTypeError(
                 "Percentage type not supported: %s" % fields["type"]
