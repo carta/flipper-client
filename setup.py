fc17385e0379bc2d59fc3f6461bb448070a43250
Adam Savitzky <adam.savitzky@gmail.com>
2019-10-28 10:11:26 -0700

    Bump version to v1.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setup(
     name="flipper-client",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     license="Apache License 2.0",
     long_description=open("README.md").read(),
