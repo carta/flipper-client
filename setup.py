d551ba8d2135fe7ca9c114ff905521197b06dd75
Mark Meleka <mark.meleka@carta.com>
2025-05-23 20:25:11 -0700

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setup(
     name="flipper-client",
-    version="1.3.2",
+    version="1.4.0",
     packages=find_packages(),
     license="Apache License 2.0",
     long_description=open("README.md").read(),
